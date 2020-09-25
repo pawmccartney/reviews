54357db9ae419c012b1016f4eb38eccbb4472290
unknown <tak@ucsd.com>
2020-09-25 13:45:46 -0700

    created connnection with mongodb

--- database/index.js
+++ database/index.js
@@ -1,12 +1,33 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test', {useMongoClient: true});
 
-
-let repoSchema = mongoose.Schema({
-  repoID: Number,
-  repoName: String,
-  owner: {login: String, id: Number},
-  repoDesc: String,
-  repoUrl: String,
-  forkCount: Number
+let reviewSchema = mongoose.Schema({
+  memberInfo: {
+    memberId: Number,
+    memberImg: String,
+    memberUserName: String,
+  },
+  reviewInfo: {
+    reviewDate: Number,
+    reviewText: String,
+    reviewPictures: {picture1: String, picture2: String},
+    contribution: Number,
+    helpful: Number
+  }
 });
+
+let Review = mongoose.model('Review', reviewSchema);
+
+var save = (review) => {
+  return new Promise((resolve, reject) => {
+    Review.create(review, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    })
+  })
+}
+
+module.exports.save = save;
