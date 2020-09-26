5ba277e859a19e71c629138b7dbcf8f610bd1a36
unknown <tak@ucsd.com>
2020-09-25 20:26:55 -0700

    created one review and uploaded to mongodb

--- database/index.js
+++ database/index.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useMongoClient: true});
+mongoose.connect('mongodb://localhost/review', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 let reviewSchema = mongoose.Schema({
   memberInfo: {
@@ -10,7 +13,7 @@ let reviewSchema = mongoose.Schema({
     memberHelpful: Number
   },
   reviewInfo: {
-    reviewDate: Number,
+    reviewDate: String,
     reviewTitle: String,
     reviewText: String,
     reviewTripType: String,
@@ -23,7 +26,7 @@ let reviewSchema = mongoose.Schema({
     responderName: String,
     responderPosition: String,
     responderPicture: String,
-    responderDate: Number,
+    responderDate: String,
     responderText: String,
     responderClose: String
   }
