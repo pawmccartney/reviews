c7b35a6e702449291b99b980f8b32c8b3be9ba8f
root <tak074@ucsd.edu>
2020-09-30 16:35:47 -0700

    removed npm start

--- test.js
+++ test.js
@@ -1,31 +1,8 @@
-const axios = require('axios');
+
 
 test('sanity check', () => {
   expect(true).toBe(true);
 });
 
-test("clien't get request is met with an array", (done) => {
-  axios.get('http://localhost:4003/reviews')
-    .then(result => {
-      expect(Array.isArray(result.data)).toBe(true);
-      done();
-    }).catch(err => {
-      done(err);
-    })
-});
 
-test("expects data from server to have the reviewer's name and review", (done) => {
-  axios.get('http://localhost:4003/reviews')
-    .then(result => {
-      let currReview = result.data[0];
-      let reviewer = currReview['memberInfo']['memberUserName'];
-      let review = currReview['reviewInfo']['reviewText'];
-
-      expect(reviewer).toBeTruthy();
-      expect(review).toBeTruthy();
-      done();
-    }).catch(err => {
-      done(err);
-    })
-});
 
