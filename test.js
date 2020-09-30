198dc6c2b4d667aed7a37e5d2dc3a5205dd6ec92
root <tak074@ucsd.edu>
2020-09-30 12:31:50 -0700

    temp2

--- test.js
+++ test.js
@@ -1,31 +1,33 @@
-// const axios = require('axios');
+const axios = require('axios');
 
 test('sanity check', () => {
   expect(true).toBe(true);
 });
 
-// test("clien't get request is met with an array", () => {
-//   axios.get('http://localhost:4003/reviews')
-//     .then(result => {
-//       expect(Array.isArray(result.data)).toBe(true);
-//     }).catch(err => {
-//       throw Error('ERROR: second test');
-//     })
-// });
+test("clien't get request is met with an array", (done) => {
+  axios.get('http://localhost:4003/reviews')
+    .then(result => {
+      expect(Array.isArray(result.data)).toBe(true);
+      done();
+    }).catch(err => {
+      done(err);
+    })
+});
 
-// test("expects data from server to have the reviewer's name and review", () => {
-//   axios.get('http://localhost:4003/reviews')
-//     .then(result => {
-//       let currReview = result.data[0];
-//       let reviewer = currReview['memberInfo']['memberUserName'];
-//       let review = currReview['reviewInfo']['reviewText'];
+test("expects data from server to have the reviewer's name and review", (done) => {
+  axios.get('http://localhost:4003/reviews')
+    .then(result => {
+      let currReview = result.data[0];
+      let reviewer = currReview['memberInfo']['memberUserName'];
+      let review = currReview['reviewInfo']['reviewText'];
 
-//       expect(reviewer).toBeTruthy();
-//       expect(review).toBeTruthy();
-//     }).catch(err => {
-//       throw Error('ERROR: third test');
-//     })
-// });
+      expect(reviewer).toBeTruthy();
+      expect(review).toBeTruthy();
+      done();
+    }).catch(err => {
+      done(err);
+    })
+});
 
 
 // test(", (done) => {
