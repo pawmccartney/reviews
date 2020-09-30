2986fef526ee0ef51beeeac0e61729652c86891c
root <tak074@ucsd.edu>
2020-09-30 11:48:43 -0700

    wrote basic tests for jest and circleCI

--- test.js
+++ test.js
@@ -1,5 +1,38 @@
-const sum = require('./server/sum');
+const axios = require('axios');
 
-test('add 1+ 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('sanity check', () => {
+  expect(true).toBe(true);
 });
+
+test("clien't get request is met with an array", () => {
+  axios.get('http://localhost:4003/reviews')
+    .then(result => {
+      expect(Array.isArray(result.data)).toBe(true);
+    }).catch(err => {
+      console.log('ERROR: ', err);
+    })
+});
+
+test("expects data from server to have the reviewer's name and review", () => {
+  axios.get('http://localhost:4003/reviews')
+    .then(result => {
+      let currReview = result.data[0];
+      let reviewer = currReview['memberInfo']['memberUserName'];
+      let review = currReview['reviewInfo']['reviewText'];
+
+      expect(reviewer).toBeTruthy();
+      expect(review).toBeTruthy();
+    }).catch(err => {
+      console.log('ERROR: ', err);
+    })
+});
+
+
+// test(", (done) => {
+//   axios.get('http://localhost:4003/reviews')
+//     .then(result => {
+
+//     }).catch(err => {
+//       done(err);
+//     })
+// });
